7248be2bcebfdf6a4b2aba474876299c1a40339d
Niveditha Sekar <sekarniveditha@gmail.com>
2023-06-30 00:03:40 +0530

    Postman Doc link update in href

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use('/admin',adminRouter)
 app.use('/mentor',mentorRouter)
 
 app.get('/', function (req, res) {
-    res.send('<h4>Postman Pusblished Doc: <a href="" target="_blank"></a></h4>')
+    res.send('<h4>Postman Pusblished Doc: <a href="https://documenter.getpostman.com/view/20660257/2s93zB4gBz" target="_blank">https://documenter.getpostman.com/view/20660257/2s93zB4gBz</a></h4>')
   })
   
   app.listen(3000)
