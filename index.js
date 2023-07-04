a3d0054bdd9195c44b2a69adc5ce6f7fd3406200
Niveditha Sekar <sekarniveditha@gmail.com>
2023-07-04 21:21:06 +0530

    port changed

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ app.get('/', function (req, res) {
     res.send('<h4>Postman Pusblished Doc: <a href="https://documenter.getpostman.com/view/20660257/2s93zB4gBz" target="_blank">https://documenter.getpostman.com/view/20660257/2s93zB4gBz</a></h4>')
   })
   
-  app.listen(3000)
+  app.listen(5000)
